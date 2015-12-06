62bc0baf14e265992f2a69751f778adc7ae6d1db
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-06 23:29:25 +0200

    Add user controller

--- app/components/users/users.ctrl.js
+++ app/components/users/users.ctrl.js
@@ -1,7 +1,12 @@
-// Define module for home controller
+// Define module for users controllers
 angular.module('app.users.controller', [])
 
-// Define home controller
+// Define users controller
 .controller('UsersController', ['$scope', 'users', function UsersController($scope, users) {
   $scope.users = users;
+}])
+
+// Define user controller
+.controller('UserController', ['$scope', 'user', function UsersController($scope, user) {
+  $scope.user = user;
 }]);
