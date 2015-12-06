bbaf707e203d6e37354aab2835fea9caeaa0f63b
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-06 23:28:56 +0200

    Add route for show user action

--- app/components/users/app.users.js
+++ app/components/users/app.users.js
@@ -1,15 +1,25 @@
-// Use this file to setup application submodule and load its dependencies
+// Use this file to setup user module and load its dependencies
 // Optinally the following files can be created and added as dependencies: 'app.users.routes', 'app.users.core', 'app.users.services', 'app.users.config'
 angular.module('app.users', ['app.users.controller', 'app.users.service'])
 
-// Setup routes
+// Setup users routes
 .config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.when('/users', {
+  $routeProvider
+  .when('/users', {
     templateUrl: 'app/components/users/users.list.tpl.html',
     controller: 'UsersController as usersCtrl',
     resolve:{
       users: ['Users', function (Users) {
         return Users.query();
+      }]
+    }
+  })
+  .when('/users/:userId', {
+    templateUrl: 'app/components/users/users.show.tpl.html',
+    controller: 'UserController as userCtrl',
+    resolve:{
+      user: ['Users', '$route', function (Users, $route) {
+        return Users.get({id: $route.current.params.userId});
       }]
     }
   });
